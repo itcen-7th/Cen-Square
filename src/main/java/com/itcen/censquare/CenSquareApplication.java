ca9e4fdc90d6de977f4a2d9ed4a3c3aeb321d087
heodongwon <ehddnjs6409@naver.com>
2025-05-20 13:20:57 +0900

    init

---
+++ src/main/java/com/itcen/censquare/CenSquareApplication.java
@@ -0,0 +1,13 @@
+package com.itcen.censquare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CenSquareApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CenSquareApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/itcen/censquare/CenSquareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itcen.censquare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CenSquareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
