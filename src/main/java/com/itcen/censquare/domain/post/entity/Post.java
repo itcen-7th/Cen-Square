c7729faa3df22eaff06f8ac7b83e6e21a1e767ce
dhfkdlsj <dhfkdlsj1@gmail.com>
2025-05-23 17:52:27 +0900

    Feat: Post 엔티티 생성
    - 게시글 정보를 저장하는 Post 엔티티 생성
    - 카테고리는 Enum 타입으로 관리
    
    Resolves: #2

--- src/main/java/com/itcen/censquare/domain/post/entity/Post.java
+++ src/main/java/com/itcen/censquare/domain/post/entity/Post.java
@@ -1,4 +1,49 @@
 package com.itcen.censquare.domain.post.entity;
 
-public class Post {
+import com.itcen.censquare.domain.post.entity.Enum.Category;
+import com.itcen.censquare.global.baseentity.TimeStampedEntity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Post extends TimeStampedEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "post_id")
+    private Long postId;
+
+//    @ManyToOne(fetch = FetchType.LAZY)
+//    @JoinColumn(name = "member_id")
+//    private Member member;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "category", nullable = false)
+    private Category category;
+
+    @Column(name = "title", nullable = false, length = 255)
+    private String title;
+
+    @Column(name = "content", columnDefinition = "TEXT")
+    private String content;
+
+    @Column(name = "view_count")
+    private Long viewCount = 0L;
+
+    @Column(name = "like_count")
+    private Long likeCount = 0L;
+
+    @Column(name = "deleted")
+    private Boolean deleted;
+
+    @Column(name = "deleted_at")
+    private LocalDateTime deletedAt;
 }
