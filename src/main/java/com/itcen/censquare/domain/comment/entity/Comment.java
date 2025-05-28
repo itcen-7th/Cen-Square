3e6ef8510f72d68a4ae5c8f99aeebaeae3df96fa
dhfkdlsj <dhfkdlsj1@gmail.com>
2025-05-28 11:28:57 +0900

    Feat: Comment 엔티티 추가
    - Comment 엔티티 추가
    
    Resolves: #15

---
+++ src/main/java/com/itcen/censquare/domain/comment/entity/Comment.java
@@ -0,0 +1,35 @@
+package com.itcen.censquare.domain.comment.entity;
+
+import com.itcen.censquare.domain.member.entity.Member;
+import com.itcen.censquare.domain.post.entity.Post;
+import com.itcen.censquare.global.baseentity.TimeStampedEntity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Comment extends TimeStampedEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "comment_id")
+    private Long commentId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id", nullable = false)
+    private Member member;
+
+    @ManyToOne
+    @JoinColumn(name = "post_id", nullable = false)
+    private Post post;
+    @Lob
+    @Column(nullable = false)
+    private String content;
+
+}
