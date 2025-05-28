7689e7e7232a252a4d96ddfc5df1970e716156ad
dhfkdlsj <dhfkdlsj1@gmail.com>
2025-05-28 13:05:03 +0900

    Feat: @EnableJpaAuditing 추가
    - JPA Auditing 기능 활성화를 위해 @EnableJpaAuditing 추가
    
    Resolves: #15

--- src/main/java/com/itcen/censquare/CenSquareApplication.java
+++ src/main/java/com/itcen/censquare/CenSquareApplication.java
@@ -2,12 +2,14 @@ package com.itcen.censquare;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class CenSquareApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(CenSquareApplication.class, args);
-    }
+  public static void main(String[] args) {
+    SpringApplication.run(CenSquareApplication.class, args);
+  }
 
 }
