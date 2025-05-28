d48d3d2a943e81bbd9c3020bb432c9571d5c0f78
dhfkdlsj <dhfkdlsj1@gmail.com>
2025-05-28 11:29:11 +0900

    Feat: Like 엔티티 추가
    - Like 엔티티 추가
    
    Resolves: #15

---
+++ src/main/java/com/itcen/censquare/domain/like/entity/Like.java
@@ -0,0 +1,32 @@
+package com.itcen.censquare.domain.like.entity;
+
+import com.itcen.censquare.domain.member.entity.Member;
+import com.itcen.censquare.domain.post.entity.Post;
+import com.itcen.censquare.global.baseentity.CreatedBaseEntity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Like extends CreatedBaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "like_id")
+    private Long likeId;
+
+    @ManyToOne
+    @JoinColumn(name = "post_id", nullable = false)
+    private Post post;
+
+    @ManyToOne
+    @JoinColumn(name = "member_id", nullable = false)
+    private Member member;
+
+}
